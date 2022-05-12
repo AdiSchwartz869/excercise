5b079258fcab91fec0c636318e854f3f70f68432
Adi Schwartz <a.schwartz@aligntech.com>
2022-05-12 09:48:44 +0300

    .

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Excercise
+# Excercise
 
 APP
 ---
--- hello.py
+++ hello.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h3>Hello Illusive</h3>"
+    return "<h3>Hello Adi</h3>"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
